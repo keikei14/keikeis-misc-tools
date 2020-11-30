8f76a91b0f192a36289af1172fd7832530a04cfd
Keikei <46136089+keikei14@users.noreply.github.com>
2020-11-30 12:01:21 +0800

    Remove useless imports

--- Tools/DbMurmurHasher/Program.cs
+++ Tools/DbMurmurHasher/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MikuMikuLibrary.IO;
 using MikuMikuLibrary.Databases;
 using MikuMikuLibrary.Hashes;
