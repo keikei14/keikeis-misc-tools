20b63f4138d11ccd6347af842c0d48ecae152308
Keikei <46136089+keikei14@users.noreply.github.com>
2020-12-10 17:30:04 +0800

    BatchXtoF2

---
+++ Tools/BatchXtoF2/BatchXtoF2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MikuMikuLibrary.IO;
+using MikuMikuLibrary.Archives;
+using MikuMikuLibrary.Databases;
+using MikuMikuLibrary.Textures;
+using MikuMikuLibrary.Textures.Processing;
+
+namespace BatchXtoF2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stgFarc = BinaryFile.Load<FarcArchive>(args[0]);
+
+            foreach (string fileName in stgFarc)
+            {
+                if (fileName.EndsWith(".txd"))
+                {
+                    MemoryStream stream = new MemoryStream();
+                    var oldTexSet = BinaryFile.Load<TextureSet>(stgFarc.Open(fileName, EntryStreamMode.MemoryStream));
+                    var newTexSet = new TextureSet();
+
+                    foreach (var tex in oldTexSet.Textures)
+                    {
+                        Texture newTex = TextureEncoder.EncodeFromFile(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\white.dds", tex.Format, true);
+                        newTex.Id = tex.Id;
+                        newTex.Name = tex.Name;
+                        newTexSet.Textures.Add(newTex);
+
+                    }
+                    newTexSet.Endianness = oldTexSet.Endianness;
+                    newTexSet.Format = oldTexSet.Format;
+
+                    newTexSet.Save(stream, true);
+                    stgFarc.Add(fileName, stream, false, ConflictPolicy.Replace);
+                }
+            }
+            stgFarc.Save(args[0]);
+        }
+    }
+}
